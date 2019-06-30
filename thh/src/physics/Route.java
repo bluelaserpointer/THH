ebf0744ef5844461ebc40001a64deb5ff3fa31c5
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2019-06-30 14:52:00 +0800

    Update Route.java
    ・Routeクラスのデバック処理をメソッド化

--- thh/src/physics/Route.java
+++ thh/src/physics/Route.java
@@ -1,9 +1,17 @@
 package physics;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.util.LinkedList;
+
+import core.GHQ;
+import effect.Effect;
+import effect.debugEffect.DebugEffect;
 
 public class Route {
 	final LinkedList<Point> points;
+	final LinkedList<Effect> debugEffects = new LinkedList<Effect>();
 	public Route(LinkedList<Point> points) {
 		this.points = points;
 	}
@@ -18,5 +26,34 @@ public class Route {
 	}
 	public void clear() {
 		points.clear();
+	}
+	public void setDebugEffect(Color color, Stroke stroke) {
+		Point prevPoint = null;
+		for(Point nowPoint : points) {
+			if(prevPoint == null) {
+				debugEffects.add(DebugEffect.setLine(color, stroke, nowPoint.intX(), nowPoint.intY(), nowPoint.intX(), nowPoint.intY() - 10));
+			}else {
+				debugEffects.add(DebugEffect.setLine(color, stroke, prevPoint, nowPoint));
+			}
+			prevPoint = nowPoint;
+		}
+	}
+	public void removeDebugEffect() {
+		while(!debugEffects.isEmpty())
+			GHQ.deleteEffect(debugEffects.remove());
+	}
+	public void debugPaint(Color color, Stroke stroke) {
+		final Graphics2D G2 = GHQ.getGraphics2D();
+		G2.setColor(color);
+		G2.setStroke(stroke);
+		Point prevPoint = null;
+		for(Point nowPoint : points) {
+			if(prevPoint == null) {
+				G2.drawLine(nowPoint.intX(), nowPoint.intY(), nowPoint.intX(), nowPoint.intY() - 10);
+			}else {
+				G2.drawLine(prevPoint.intX(), prevPoint.intY(), nowPoint.intX(), nowPoint.intY());
+			}
+			prevPoint = nowPoint;
+		}
 	}
 }
