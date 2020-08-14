b0487308eb700ca747ef0eeec04fc3eadffe0a81
Naomi <41462684+naomixie@users.noreply.github.com>
2020-08-14 22:22:46 +0800

    fix: GamePageSwitcher

--- thh/troubleCrasher/engine/GamePageSwitcher.java
+++ thh/troubleCrasher/engine/GamePageSwitcher.java
@@ -11,43 +11,45 @@ import paint.rect.RectPaint;
 
 public class GamePageSwitcher extends GUIPartsSwitcher {
 	private static final int STARTSCREEN = 0, GAMESCREEN = 1, SETTINGSCREEN = 2;
-//	private final ImageFrame defaultSlotPaint, buttonPaint;
-//	private final RectPaint bgPaint;
-	
+	// private final ImageFrame defaultSlotPaint, buttonPaint;
+	// private final RectPaint bgPaint;
+
 	public GamePageSwitcher() {
-		super(3,STARTSCREEN);
-//		defaultSlotPaint = ImageFrame.create("picture/banana.png");
-//		buttonPaint = ImageFrame.create("picture/banana.png");
-//		bgPaint = new RectPaint() {
-//			private final ImageFrame
-//				bg1 = ImageFrame.create("picture/banana.png"),
-//				bg2 = ImageFrame.create("picture/banana.png");
-//			@Override
-//			public void rectPaint(int x, int y, int w, int h) {
-//				bg1.rectPaint(x, y, w, h);
-//				bg2.rectPaint(x, y, w, h);
-//			}
-//		};
-//		this.setBGPaint(bgPaint);
-//		this.appendLast(new AutoResizeMenu(0, 0, GHQ.screenW(), 70) {
-//			final GUIParts 
-//				gameScrBtn = getSwitcherButton(GAMESCREEN).setBGPaint(buttonPaint).setName("inventoryScrBtn"),
-//				settingsScrBtn = getSwitcherButton(SETTINGSCREEN).setBGPaint(buttonPaint).setName("talentScrBtn");
-//			{
-//				this.addNewLine(gameScrBtn, settingsScrBtn);
-//				this.setName("START_MENU_TOP_TAB");
-//			}
-//		});
-		
+		super(3, STARTSCREEN);
+		// defaultSlotPaint = ImageFrame.create("picture/banana.png");
+		// buttonPaint = ImageFrame.create("picture/banana.png");
+		// bgPaint = new RectPaint() {
+		// private final ImageFrame
+		// bg1 = ImageFrame.create("picture/banana.png"),
+		// bg2 = ImageFrame.create("picture/banana.png");
+		// @Override
+		// public void rectPaint(int x, int y, int w, int h) {
+		// bg1.rectPaint(x, y, w, h);
+		// bg2.rectPaint(x, y, w, h);
+		// }
+		// };
+		// this.setBGPaint(bgPaint);
+		// this.appendLast(new AutoResizeMenu(0, 0, GHQ.screenW(), 70) {
+		// final GUIParts
+		// gameScrBtn =
+		// getSwitcherButton(GAMESCREEN).setBGPaint(buttonPaint).setName("inventoryScrBtn"),
+		// settingsScrBtn =
+		// getSwitcherButton(SETTINGSCREEN).setBGPaint(buttonPaint).setName("talentScrBtn");
+		// {
+		// this.addNewLine(gameScrBtn, settingsScrBtn);
+		// this.setName("START_MENU_TOP_TAB");
+		// }
+		// });
+
 		set(STARTSCREEN, new GUIParts() {
-//			final ImageFrame StartScreen = ImageFrame.create("picture/banana.png");
+			// final ImageFrame StartScreen = ImageFrame.create("picture/banana.png");
 			{
 				setName("STARTSCREEN");
 				setBGColor(Color.GREEN);
-				
+
 			}
 		});
-		set(GAMESCREEN,new GUIParts() {
+		set(GAMESCREEN, new GUIParts() {
 			{
 				setName("GAMESCREEN");
 				setBGColor(Color.BLUE);
@@ -55,10 +57,12 @@ public class GamePageSwitcher extends GUIPartsSwitcher {
 		});
 		set(SETTINGSCREEN, new GUIParts() {
 			{
-			setName("SETTINGSCREEN");
-			setBGColor(Color.red);
+				setName("SETTINGSCREEN");
+				setBGColor(Color.red);
 			}
 		});
+
+		GHQ.addGUIParts(this);
 	}
 
 }
--- thh/troubleCrasher/engine/TCGame.java
+++ thh/troubleCrasher/engine/TCGame.java
@@ -7,21 +7,23 @@ import core.Game;
 import physics.stage.GHQStage;
 
 public class TCGame extends Game {
-	
+
 	public static void main(String args[]) {
 		new GHQ(new TCGame(), 1000, 600);
 	}
-	
+
 	public TCGame() {
-		super(new GamePageSwitcher()); //TODO: change it!!
+		super(new GamePageSwitcher());
 	}
+
 	@Override
 	public GHQStage loadStage() {
 		return null;
 	}
+
 	@Override
 	public void idle(Graphics2D g2, int stopEventKind) {
-		
+
 	}
 
 }
