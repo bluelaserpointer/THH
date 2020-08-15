405e47ee80ac7f79d43b2be2047efbcecbf07da9
edwardraymondhe <edward.raymond.he@hotmail.com>
2020-08-15 10:47:53 +0800

    feat: added text prototypes and scriptManager

--- thh/troubleCrasher/engine/TCGame.java
+++ thh/troubleCrasher/engine/TCGame.java
@@ -1,25 +1,33 @@
 package troubleCrasher.engine;
 
 import java.awt.Graphics2D;
+import java.io.File;
+import java.io.IOException;
 
 import core.GHQ;
 import core.Game;
 import gui.GUIPartsSwitcher;
 import physics.stage.GHQStage;
+import troubleCrasher.story.*;
 
 public class TCGame extends Game {
 	
-	public static void main(String args[]) {
+	
+	public static void main(String args[]) throws IOException {
 		new GHQ(new TCGame(), 1000, 600);
 	}
 	
-	public TCGame() {
+	public TCGame() throws IOException {
 		super(new GUIPartsSwitcher(0, 0)); //TODO: change it!!
+		
+		ScriptManager scriptManager = new ScriptManager("1");
 	}
+		
 	@Override
 	public GHQStage loadStage() {
 		return null;
 	}
+	
 	@Override
 	public void idle(Graphics2D g2, int stopEventKind) {
 		
--- thh/troubleCrasher/story/ScriptManager.java
+++ thh/troubleCrasher/story/ScriptManager.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 // Parsing scripts
 public class ScriptManager {
@@ -12,21 +15,45 @@ public class ScriptManager {
 	// public int type;
 	
     public BufferedReader buffReader; 
+    private String pathToScripts = "troubleCrasher/story/scripts/";
+    private String currentFile = "";
+    
+    public List<String> currentOptions = new ArrayList();
 	
 	public ScriptManager() {}
+	
+	public ScriptManager(String fileName) throws IOException {
+		this.currentFile = fileName;
+		this.parseFile(fileName);
+	}
+	
+	public String parsePath(String name)
+	{
+		return pathToScripts + name + ".txt";
+	}
 		
 	/**
 	 * Basic method for parsing a single file
 	 * @param file Current parsing file
 	 * @throws IOException 
 	 */
-	public void parseFile(String filePath) throws IOException {
+	public void parseFile(String fileName) throws IOException {
+		String filePath = parsePath(fileName);
+		
 	    FileInputStream fin = new FileInputStream(filePath);
 	    InputStreamReader reader = new InputStreamReader(fin);
 	    buffReader = new BufferedReader(reader);
 	    String currLine = "";
 	    while((currLine = buffReader.readLine())!=null){
-	    	parseLine(currLine);
+	    	try {
+				parseLine(currLine);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	    }
 	    buffReader.close();
 	}
@@ -35,14 +62,29 @@ public class ScriptManager {
 	 * Main method for looping through the lines
 	 * @param line Current parsing line
 	 * @throws IOException 
+	 * @throws InterruptedException 
 	 */
-	public void parseLine(String currLine) throws IOException {
+	public void parseLine(String currLine) throws IOException, InterruptedException {
+		System.out.println(currLine);
 		if(currLine.charAt(0) == '#')
 		{
 			parseFunc(currLine);
 		}
 		if((currLine = buffReader.readLine()) != null)
+		{
+			waitLine();
 			parseLine(currLine);
+		}
+	}
+	
+	/**
+	 * Simply waits for user signal, such as touching screen
+	 * @throws InterruptedException 
+	 */
+	private void waitLine() throws InterruptedException
+	{
+		Thread.currentThread();
+		Thread.sleep(0);
 	}
 	
 	
@@ -50,12 +92,13 @@ public class ScriptManager {
 	 * Parses function
 	 * @param funcLine
 	 * @throws IOException
+	 * @throws InterruptedException 
 	 */
-	public void parseFunc(String funcLine) throws IOException
+	public void parseFunc(String funcLine) throws IOException, InterruptedException
 	{		
 		String[] parsedLine = funcLine.split("#");
-		
-		switch(parsedLine[0])
+		// System.out.println("In parseFunc");
+		switch(parsedLine[1])
 		{
 			case "OPTIONS":
 				parseOptions();
@@ -65,11 +108,22 @@ public class ScriptManager {
 				break;
 			
 			// TODO: Haven't started yet
+			case "STARTLINE":
+			case "ENDLINE":
+				break;	
 			case "LIST":
 				break;
 			case "ADD":
 				break;
 			case "DELETE":
+				break;
+			default:
+				if(!funcLine.contains("##"))
+				{
+					System.out.println("!!ERROR!!: " + funcLine);
+					break;
+				}
+				System.out.println("!!COMMENT!!: " + funcLine);
 				break;
 		}
 	}
@@ -78,16 +132,17 @@ public class ScriptManager {
 	/**
 	 * Parses if statements
 	 * @throws IOException 
+	 * @throws InterruptedException 
 	 */
-	private void parseIf(String funcLine) throws IOException {
-		String currLine = buffReader.readLine();
-		if(fulfillIf(funcLine))
-		{
-			// If statement is fulfilled
-			parseLine(currLine);
-		}else {
-			skipIf();
-		}
+	private void parseIf(String funcLine) throws IOException, InterruptedException {
+		String[] parsedLine = funcLine.split("#");
+		// Mocking result (true or false)
+		// System.out.println("In parseIf fulFill: " + funcLine);
+		Scanner scan = new Scanner(System.in);  //创建Scanner扫描器来封装System类的in输入流
+        int fulfill = scan.nextInt();
+		
+		if(!mockFulfillIf(fulfill))
+			skipIf(Integer.parseInt(parsedLine[2]));
 	}
 	
 
@@ -100,26 +155,94 @@ public class ScriptManager {
 		return false;
 	}
 	
+	/** Checks if the statement is fulfilled
+	 * @param funcLine
+	 * @return
+	 */
+	private boolean mockFulfillIf(int mock) {
+		return mock == 1;
+	}
+	
 	
 	/**
 	 * Skips the statements within if
 	 * @throws IOException
 	 */
-	private void skipIf() throws IOException
+	private void skipIf(int index) throws IOException
 	{
 		String currLine = "";
-		while((currLine = buffReader.readLine()).split("#")[0] != "ENDIF")
+		String matchIf = "#ENDIF#" + index;
+		// System.out.println("In skipIf");
+		while(!(currLine = buffReader.readLine()).contains(matchIf))
 		{
 			System.out.println(currLine);
 		}
 	}
 	
+	private void optionsInit()
+	{
+		this.currentOptions.clear();
+	}
+	
 	
 	/**
 	 * Parses option groups, redirects to another file
+	 * @throws IOException 
+	 * @throws InterruptedException 
 	 */
-	private void parseOptions() {
-		// Redirects to another file
+	private void parseOptions() throws IOException, InterruptedException {
+		int index = 0;
+		
+		String currLine = "";
+		optionsInit();
+		
+		// System.out.println("In parseOptions");
+		while(!(currLine = buffReader.readLine()).contains("#ENDOPTIONS"))
+		{
+			if(currLine.charAt(0) == '#')
+			{
+				if(currLine.contains("#OPTION"))
+				{
+					// Add nextLine to string array
+					currentOptions.add(buffReader.readLine());
+					index++;
+					continue;
+				}else if(currLine.contains("#IF"))
+				{
+					parseIf(currLine);
+					continue;
+				}
+			}
+			if(currLine == "#END")
+			{
+				continue;
+			}	
+		}
+		
+		// Pauses for player to send signal, indexes must be inside the existing options
+//		System.out.println("size " + currentOptions.size());
+		if(currentOptions.size() > 0)
+		{
+			Scanner scan = new Scanner(System.in);  //创建Scanner扫描器来封装System类的in输入流
+	        int optionIndex = scan.nextInt();
+	        chooseOption(optionIndex);
+		}
 	}
 
+	/**
+	 * Chooses option
+	 * @param index
+	 * @throws IOException
+	 * @throws InterruptedException 
+	 */
+	public void chooseOption(int index) throws IOException, InterruptedException
+	{
+		System.out.println("Choosing option " + index);
+		
+		ScriptManager scriptManager = new ScriptManager(currentFile + "-" + index);
+		
+		// Scripts after optionGroup
+		parseLine(buffReader.readLine());
+	}
+	
 }
