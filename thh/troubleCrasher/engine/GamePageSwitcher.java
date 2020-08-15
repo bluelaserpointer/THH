bd6a6f5c0c2d16dabf40da43543df33e2b2e5617
Naomi <41462684+naomixie@users.noreply.github.com>
2020-08-15 12:53:33 +0800

    feat: GamePageSwitcher

---
+++ thh/troubleCrasher/engine/GamePageSwitcher.java
@@ -0,0 +1,303 @@
+package troubleCrasher.engine;
+
+import java.awt.Color;
+
+import core.GHQ;
+import gui.AutoResizeMenu;
+import gui.GUIParts;
+import gui.GUIPartsSwitcher;
+import paint.ImageFrame;
+import paint.rect.RectPaint;
+
+public class GamePageSwitcher extends GUIPartsSwitcher {
+	private static final int STARTSCREEN = 0, GAMESCREEN = 1, SETTINGSCREEN = 2;
+	private static final int PROFILE_SESSION = 0, BOX_SESSION = 1, SETTING_SESSION = 2, SAVE_SESSION = 3;
+	private static final int WORK_SCENE = 0, HOME_SCENE = 1, BAR_SCENE = 2; // DIFFERENT SCENES
+	private static final int NO_ONE = 0, NPC_1 = 1, NPC_2 = 2, NPC_3 = 3, NPC_4 = 4; // DIFFERENT NPCS
+	private final ImageFrame defaultSlotPaint, buttonPaint;
+	private static final String StartScreenPic = "thhimage/WhiteBall.png";
+	private int centerW, centerH;
+
+	public GamePageSwitcher() {
+		super(3, STARTSCREEN);
+		defaultSlotPaint = ImageFrame.create("thhimage/MillkyWay.png");
+		buttonPaint = ImageFrame.create("thhimage/veg_leaf3.png"); // 按钮图片
+		centerH = 384;
+		centerW = 512;
+
+		// Start Menu
+		set(STARTSCREEN, new GUIParts() {
+
+			{
+				this.setBGImage(StartScreenPic);
+				setName("STARTSCREEN");
+				// setBGColor(Color.BLACK);
+
+				this.appendLast(new GUIParts() {
+
+					final GUIParts gameScrBtn = getSwitcherButton(GAMESCREEN).setBGPaint(buttonPaint)
+							.setName("gameScrBtn").setBounds(centerW - 150, 200, 300, 100),
+							settingsScrBtn = getSwitcherButton(SETTINGSCREEN).setBGPaint(buttonPaint)
+									.setName("settingScrBtn").setBounds(centerW - 150, 350, 300, 100);
+					// quitScrBtn = getSwitcherButton(SETTINGSCREEN).setBGPaint(buttonPaint)
+					// .setName("settingScrBtn").setBounds(512, 400, 100, 200);
+					// TODO: Add quit
+					// button
+					{
+						// this.addNewLine(gameScrBtn, settingsScrBtn);
+						this.setName("START_MENU_TOP_TAB");
+						this.appendFirst(gameScrBtn);
+						this.appendFirst(settingsScrBtn);
+
+					}
+
+				});
+
+			}
+
+			// @Override
+			// public void idle() {
+			// super.idle();
+			// // StartScreen.dotPaint_rate(200, 200, 1.2);
+			// }
+		});
+
+		// GAME SCREEN
+		set(GAMESCREEN, new GUIParts() {
+			{
+				setName("GAMESCREEN");
+				// 游戏功能栏及其展开画面
+				this.appendLast(new GUIPartsSwitcher(4, PROFILE_SESSION) {
+					{
+						setName("ACTIVITY_SESSION");
+
+						this.appendLast(new GUIParts() {
+
+							final GUIParts profileScrBtn = getSwitcherButton(PROFILE_SESSION).setBGPaint(buttonPaint)
+									.setName("profileScrBtn").setBounds(5, 20, 60, 60),
+									boxScrBtn = getSwitcherButton(BOX_SESSION).setBGPaint(buttonPaint)
+											.setName("boxScrBtn").setBounds(5, 90, 60, 60),
+									settingScrBtn = getSwitcherButton(SETTING_SESSION).setBGPaint(buttonPaint)
+											.setName("settingScrBtn").setBounds(5, 160, 60, 60),
+									saveScrBtn = getSwitcherButton(SAVE_SESSION).setBGPaint(buttonPaint)
+											.setName("saveScrBtn").setBounds(5, 230, 60, 60);
+							// quitScrBtn = getSwitcherButton(SETTINGSCREEN).setBGPaint(buttonPaint)
+							// .setName("settingScrBtn").setBounds(512, 400, 100, 200);
+							// TODO: Add quit
+							// button
+							{
+								// this.addNewLine(gameScrBtn, settingsScrBtn);
+								this.setName("START_MENU_TOP_TAB");
+								this.appendFirst(profileScrBtn);
+								this.appendFirst(boxScrBtn);
+								this.appendFirst(settingScrBtn);
+								this.appendFirst(saveScrBtn);
+
+							}
+
+						});
+
+						set(PROFILE_SESSION, new GUIPartsSwitcher(5, NPC_1) {
+							{
+								setName("PROFILE_SESSION");
+								this.appendLast(new GUIParts() {
+									final GUIParts NPC_1ScrBtn = getSwitcherButton(NPC_1).setBGPaint(buttonPaint)
+											.setName("NPC_1ScrBtn").setBounds(75, 20, 60, 60),
+											NPC_2ScrBtn = getSwitcherButton(NPC_2).setBGPaint(buttonPaint)
+													.setName("NPC_2ScrBtn").setBounds(75, 90, 60, 60),
+											NPC_3ScrBtn = getSwitcherButton(NPC_3).setBGPaint(buttonPaint)
+													.setName("NPC_3ScrBtn").setBounds(75, 160, 60, 60),
+											NPC_4ScrBtn = getSwitcherButton(NPC_4).setBGPaint(buttonPaint)
+													.setName("NPC_4ScrBtn").setBounds(75, 230, 60, 60);
+									// setBGColor(Color.red);
+									// setBounds(70, 0, 360, 768);
+									{
+										// this.addNewLine(gameScrBtn, settingsScrBtn);
+										this.setName("PROFILE_MENU_TOP_TAB");
+										this.appendLast(NPC_1ScrBtn);
+										this.appendLast(NPC_2ScrBtn);
+										this.appendLast(NPC_3ScrBtn);
+										this.appendLast(NPC_4ScrBtn);
+
+									}
+								});
+								set(NPC_1, new GUIParts() {
+									{
+										setName("NPC_1_PROFILE");
+										setBGColor(Color.lightGray);
+										setBounds(140, 0, 290, 768);
+									}
+								});
+								set(NPC_2, new GUIParts() {
+									{
+										setName("NPC_2_PROFILE");
+										setBGColor(Color.orange);
+										setBounds(140, 0, 290, 768);
+									}
+								});
+								set(NPC_3, new GUIParts() {
+									{
+										setName("NPC_3_PROFILE");
+										setBGColor(Color.pink);
+										setBounds(140, 0, 290, 768);
+									}
+								});
+								set(NPC_4, new GUIParts() {
+									{
+										setName("NPC_4_PROFILE");
+										setBGColor(Color.green);
+										setBounds(140, 0, 290, 768);
+									}
+								});
+							}
+						});
+
+						set(BOX_SESSION, new GUIParts() {
+							{
+								setName("BOX_SESSION");
+								setBGColor(Color.yellow);
+								setBounds(70, 0, 360, 768);
+							}
+						});
+						set(SETTING_SESSION, new GUIParts() {
+							{
+								setName("SETTING_SESSION");
+								setBGColor(Color.MAGENTA);
+								setBounds(70, 0, 360, 768);
+							}
+						});
+						set(SAVE_SESSION, new GUIParts() {
+							{
+								setName("SAVE_SESSION");
+								setBGColor(Color.pink);
+								setBounds(70, 0, 360, 768);
+							}
+						});
+
+						// setBGColor(Color.BLUE);
+					}
+				});
+
+				// 场景画面
+				this.appendLast(new GUIParts() {
+					{
+						this.appendLast(new GUIPartsSwitcher(4, WORK_SCENE) {
+							{
+								setName("SCENE_SESSION");
+
+								this.appendLast(new GUIParts() {
+
+									final GUIParts workScrBtn = getSwitcherButton(WORK_SCENE).setBGPaint(buttonPaint)
+											.setName("workScrBtn").setBounds(430, 20, 60, 60),
+											barScrBtn = getSwitcherButton(BAR_SCENE).setBGPaint(buttonPaint)
+													.setName("barScrBtn").setBounds(430, 90, 60, 60),
+											homeScrBtn = getSwitcherButton(HOME_SCENE).setBGPaint(buttonPaint)
+													.setName("homeScrBtn").setBounds(430, 160, 60, 60);
+
+									// quitScrBtn = getSwitcherButton(SETTINGSCREEN).setBGPaint(buttonPaint)
+									// .setName("settingScrBtn").setBounds(512, 400, 100, 200);
+									// button
+									{
+										// this.addNewLine(gameScrBtn, settingsScrBtn);
+										this.setName("SCENE_MENU_SWTCHER_TAB");
+										this.appendFirst(workScrBtn);
+										this.appendFirst(barScrBtn);
+										this.appendFirst(homeScrBtn);
+
+									}
+
+								});
+
+								set(WORK_SCENE, new GUIParts() {
+									{
+										setName("WORK_SCENE");
+										setBGColor(Color.blue);
+										setBounds(430, 0, 594, 520);
+									}
+								});
+
+								// 设定
+								// 存档
+								// 箱子
+
+								set(BAR_SCENE, new GUIParts() {
+									{
+										setName("BAR_SCENE");
+										setBGColor(Color.orange);
+										setBounds(430, 0, 594, 520);
+									}
+								});
+
+								set(HOME_SCENE, new GUIParts() {
+									{
+										setName("HOME_SCENE");
+										setBGColor(Color.GRAY);
+										setBounds(430, 0, 594, 520);
+									}
+								});
+								// setBGColor(Color.BLUE);
+							}
+						});
+
+						// NPC
+						this.appendFirst(new GUIPartsSwitcher(5, NPC_1) {
+							{
+								setName("SCENE_SESSION");
+
+								set(NO_ONE, new GUIParts() {
+									{
+										// NO MPC IN SCENE
+									}
+								});
+
+								set(NPC_1, new GUIParts() {
+									{
+										setName("NPC_1");
+										setBGColor(Color.orange);
+										setBounds(627, 220, 200, 300);
+									}
+								});
+
+								set(NPC_2, new GUIParts() {
+									{
+										setName("NPC_2");
+										setBGColor(Color.GRAY);
+										setBounds(627, 220, 200, 300);
+									}
+								});
+								set(NPC_3, new GUIParts() {
+									{
+										setName("NPC_3");
+										setBGColor(Color.pink);
+										setBounds(627, 220, 200, 300);
+									}
+								});
+								set(NPC_4, new GUIParts() {
+									{
+										setName("NPC_4");
+										setBGColor(Color.cyan);
+										setBounds(627, 220, 200, 300);
+									}
+								});
+
+								// setBGColor(Color.BLUE);
+							}
+						});
+
+					}
+				});
+
+			}
+		});
+
+		set(SETTINGSCREEN, new GUIParts() {
+			{
+				setName("SETTINGSCREEN");
+				setBGColor(Color.red);
+			}
+		});
+
+		GHQ.addGUIParts(this);
+	}
+
+}
--- thh/troubleCrasher/engine/TCGame.java
+++ thh/troubleCrasher/engine/TCGame.java
@@ -20,43 +20,48 @@ import troubleCrasher.jigsaw.JigsawEnum;
 import troubleCrasher.jigsaw.JigsawViewer;
 
 public class TCGame extends Game {
-	
+
 	private JigsawViewer jigsawViewer;
 	private Jigsaw testJigsaw;
+
 	public static void main(String args[]) throws IOException {
-		new GHQ(new TCGame(), 1000, 600);
+		new GHQ(new TCGame(), 1024, 768);
 	}
-	
+
 	public TCGame() throws IOException {
-		super(new GUIPartsSwitcher(0, 0)); //TODO: change it!!
-		
-    // Scripts
+		super(new GamePageSwitcher()); // TODO: change it!!
+
+		// Scripts
 		// ScriptManager scriptManager = new ScriptManager("1");
-    
-		//UI
-//		jigsawViewer = new JigsawViewer(5, 5);
-//		jigsawViewer.setBGColor(Color.BLACK);
-//		jigsawViewer.setBounds(0, 0, 75*5, 75*5);
-//		jigsawViewer.board().setJigsaw(testJigsaw = new Jigsaw(JigsawEnum.L32).setGridPos(1, 1));
-//		GHQ.addGUIParts(jigsawViewer);
-//		GHQ.addGUIParts(new UIShowHideButton(jigsawViewer)).setBounds(750, 50, 100, 100).setBGColor(Color.LIGHT_GRAY);
+
+		// UI
+		// jigsawViewer = new JigsawViewer(5, 5);
+		// jigsawViewer.setBGColor(Color.BLACK);
+		// jigsawViewer.setBounds(0, 0, 75*5, 75*5);
+		// jigsawViewer.board().setJigsaw(testJigsaw = new
+		// Jigsaw(JigsawEnum.L32).setGridPos(1, 1));
+		// GHQ.addGUIParts(jigsawViewer);
+		// GHQ.addGUIParts(new UIShowHideButton(jigsawViewer)).setBounds(750, 50, 100,
+		// 100).setBGColor(Color.LIGHT_GRAY);
 	}
+
 	@Override
 	public String getTitleName() {
 		return "TroubleCrusher";
 	}
-		
+
 	@Override
 	public GHQStage loadStage() {
 		return null;
 	}
-	
+
 	@Override
 	public void idle(Graphics2D g2, int stopEventKind) {
-//		if(GHQ.nowFrame() % 10 == 0) {
-//			testJigsaw.setDirection(testJigsaw.direction().right());
-//		}
+		// if(GHQ.nowFrame() % 10 == 0) {
+		// testJigsaw.setDirection(testJigsaw.direction().right());
+		// }
 	}
+
 	protected Camera starterCamera() {
 		return null;
 	}
