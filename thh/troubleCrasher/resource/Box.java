742b58df1e75b6e29889c5abd28f111782bf8073
edwardraymondhe <edward.raymond.he@hotmail.com>
2020-08-15 12:06:08 +0800

    feat: basic resource

---
+++ thh/troubleCrasher/resource/Box.java
@@ -0,0 +1,24 @@
+package troubleCrasher.resource;
+
+import troubleCrasher.jigsaw.Jigsaw;
+import troubleCrasher.jigsaw.JigsawEnum;
+
+
+public class Box extends Jigsaw {
+	private String boxName;
+	
+	public Box(String boxName, JigsawEnum type) {
+		super(type);
+		this.boxName = boxName;
+	}
+	
+	public void setBoxName(String boxName)
+	{
+		this.boxName = boxName;
+	}
+	
+	public String getBoxName() {
+		return this.boxName;
+	}
+	
+}
