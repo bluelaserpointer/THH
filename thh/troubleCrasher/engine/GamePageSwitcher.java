1ca2554bb848b1e60fb7b6e1e70dbd647bd85288
Naomi <41462684+naomixie@users.noreply.github.com>
2020-08-15 19:15:56 +0800

    wip: 整合美工

--- thh/troubleCrasher/engine/GamePageSwitcher.java
+++ thh/troubleCrasher/engine/GamePageSwitcher.java
@@ -14,20 +14,17 @@ import gui.GUIParts;
 import gui.GUIPartsSwitcher;
 import paint.ImageFrame;
 import paint.rect.RectPaint;
+import troubleCrasher.person.PersonEnum;
+import troubleCrasher.person.SceneEnum;
 
 public class GamePageSwitcher extends GUIPartsSwitcher {
 	private static final int STARTSCREEN = 0, GAMESCREEN = 1, SETTINGSCREEN = 2;
 	private static final int PROFILE_SESSION = 0, BOX_SESSION = 1, SETTING_SESSION = 2, SAVE_SESSION = 3;
-	private static final int WORK_SCENE_DAY = 0, WORK_SCENE_NIGHT = 1, BAR_SCENE = 2; // DIFFERENT SCENES
 	private static final int NO_ONE = -1, CAPTAIN = 0, FARMER = 1, DOCTOR = 2, ANNOUNCER = 3; // DIFFERENT NPCS
 	// private final ImageFrame buttonPaint;
-	private String[] SceneBG = { "thhimage/OfficeDay.png", "thhimage/OfficeNight.png", "thhimage/Bar.png" };
-	private String[] NPCImg = { "thhimage/Captain.png", "thhimage/Farmer.png", "thhimage/Doctor.png",
-			"thhimage/Announcer.png" };
 
-	private GUIParts nextButton, DIALOGUE_SECTION, NPC_PART, SCENE_PART;
+	private GUIParts nextButton, NPC_PART, SCENE_PART;
 	private AnimatedGHQTextArea Dialogue, Speaker;
-	private int centerW, centerH;
 
 	public GamePageSwitcher() {
 		super(3, STARTSCREEN);
@@ -37,48 +34,48 @@ public class GamePageSwitcher extends GUIPartsSwitcher {
 		Speaker = (AnimatedGHQTextArea) new AnimatedGHQTextArea().setBounds(477, 540, 500, 30).setBGColor(Color.yellow)
 				.disable();
 		nextButton = new GUIParts().setBGColor(Color.pink).setBounds(900, 698, 100, 50);
-		System.out.println(SceneBG[WORK_SCENE_DAY]);
-		NPC_PART = new GUIParts().setName("NPC_IMAGE").setBounds(627, 220, 200, 300).setBGImage(NPCImg[CAPTAIN]);
+		NPC_PART = new GUIParts().setName("NPC_IMAGE").setBounds(627, 220, 200, 300)
+				.setBGImage(PersonEnum.CAPTAIN.personImage);
 		SCENE_PART = new GUIParts().setName("SCENE_PART").setBounds(430, 0, 594, 520)
-				.setBGImage(SceneBG[WORK_SCENE_DAY]);
+				.setBGImage(SceneEnum.WORK_DAY.sceneImage);
 
-		DIALOGUE_SECTION = new GUIParts().setBGColor(Color.green).setBounds(430, 520, 1024, 768).appendLast(Speaker)
-				.appendLast(Dialogue.setText(
-						"1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")
-						.enable())
-				.appendLast(nextButton);
-
-		centerH = 384;
-		centerW = 512;
+		// DIALOGUE_SECTION = new GUIParts().setBGColor(Color.green).setBounds(430, 520,
+		// 1024, 768).appendLast(Speaker)
+		// .appendLast(Dialogue.setText(
+		// "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")
+		// .enable())
+		// .appendLast(nextButton);
 
 		// Start Menu
 		set(STARTSCREEN, new GUIParts() {
-
 			{
-				this.setBGImage(SceneBG[WORK_SCENE_DAY]);
+				this.setBGImage("thhimage/Main_Menu_BG.png");
 				setName("STARTSCREEN");
-				// setBGColor(Color.BLACK);
 
 				this.appendLast(new GUIParts() {
 
-					final GUIParts gameScrBtn = getSwitcherButton(GAMESCREEN).setBGPaint(buttonPaint)
-							.setName("gameScrBtn").setBounds(centerW - 150, 200, 300, 100),
-							settingsScrBtn = getSwitcherButton(SETTINGSCREEN).setBGPaint(buttonPaint)
-									.setName("settingScrBtn").setBounds(centerW - 150, 350, 300, 100);
-					// quitScrBtn = getSwitcherButton(SETTINGSCREEN).setBGPaint(buttonPaint)
+					final GUIParts newgameScrBtn = getSwitcherButton(GAMESCREEN)
+							.setBGImage("thhimage/Main_Menu_NewGame.png").setName("newgameScrBtn")
+							.setBounds(730, 320, 230, 50),
+							loadgameScrBtn = getSwitcherButton(SETTINGSCREEN)
+									.setBGImage("thhimage/Main_Menu_LoadGame.png").setName("loadgameScrBtn")
+									.setBounds(720, 410, 250, 50),
+							settingsScrBtn = getSwitcherButton(SETTINGSCREEN)
+									.setBGImage("thhimage/Main_Menu_Setting.png").setName("settingScrBtn")
+									.setBounds(745, 500, 200, 50);
+
+					// quitScrBtn =
+					// getSwitcherButton(SETTINGSCREEN).setBGImage("thhimage/veg_leaf2.png")
 					// .setName("settingScrBtn").setBounds(512, 400, 100, 200);
 					// TODO: Add quit
 					// button
 					{
-						// this.addNewLine(gameScrBtn, settingsScrBtn);
-						this.setName("START_MENU_TOP_TAB");
-						this.appendFirst(gameScrBtn);
+						this.setName("START_MENU_TABS");
+						this.appendFirst(newgameScrBtn);
 						this.appendFirst(settingsScrBtn);
-
+						this.appendFirst(loadgameScrBtn);
 					}
-
 				});
-
 			}
 
 			// @Override
@@ -96,88 +93,114 @@ public class GamePageSwitcher extends GUIPartsSwitcher {
 				this.appendLast(new GUIPartsSwitcher(4, PROFILE_SESSION) {
 					{
 						setName("ACTIVITY_SESSION");
+						this.setBounds(0, 0, 70, 768);
+						this.setBGImage("thhimage/UtilityBar.png");
 
 						this.appendLast(new GUIParts() {
 
-							final GUIParts profileScrBtn = getSwitcherButton(PROFILE_SESSION).setBGPaint(buttonPaint)
-									.setName("profileScrBtn").setBounds(5, 20, 60, 60),
-									boxScrBtn = getSwitcherButton(BOX_SESSION).setBGPaint(buttonPaint)
-											.setName("boxScrBtn").setBounds(5, 90, 60, 60),
-									settingScrBtn = getSwitcherButton(SETTING_SESSION).setBGPaint(buttonPaint)
-											.setName("settingScrBtn").setBounds(5, 160, 60, 60),
-									saveScrBtn = getSwitcherButton(SAVE_SESSION).setBGPaint(buttonPaint)
-											.setName("saveScrBtn").setBounds(5, 230, 60, 60);
-							// quitScrBtn = getSwitcherButton(SETTINGSCREEN).setBGPaint(buttonPaint)
+							final GUIParts profileScrBtn = getSwitcherButton(PROFILE_SESSION)
+									.setBGImage("thhimage/UtilityBar_Profile_Chosen.png").setName("profileScrBtn")
+									.setBounds(10, 10, 50, 50),
+									boxScrBtn = getSwitcherButton(BOX_SESSION)
+											.setBGImage("thhimage/UtilityBar_Resource.png").setName("boxScrBtn")
+											.setBounds(10, 80, 50, 50),
+									settingScrBtn = getSwitcherButton(SETTING_SESSION)
+											.setBGImage("thhimage/UtilityBar_Settings.png").setName("settingScrBtn")
+											.setBounds(10, 160, 50, 50),
+									saveScrBtn = getSwitcherButton(SAVE_SESSION)
+											.setBGImage("thhimage/UtilityBar_Save.png").setName("saveScrBtn")
+											.setBounds(10, 230, 50, 50);
+							// quitScrBtn =
+							// getSwitcherButton(SETTINGSCREEN).setBGImage("thhimage/veg_leaf2.png")
 							// .setName("settingScrBtn").setBounds(512, 400, 100, 200);
 							// TODO: Add quit
 							// button
 							{
 								// this.addNewLine(gameScrBtn, settingsScrBtn);
-								this.setName("GAME_MENU_TOP_TAB");
-								this.setBGColor(Color.RED);
-								this.setBounds(0, 0, 70, 768);
+								this.setName("PROFILE_MENU_TAB");
 								this.appendFirst(profileScrBtn);
 								this.appendFirst(boxScrBtn);
 								this.appendFirst(settingScrBtn);
 								this.appendFirst(saveScrBtn);
-								this.setBounds(0, 0, 70, 768);
 
 							}
 
 						});
 
-						set(PROFILE_SESSION, new GUIPartsSwitcher(5, NPC_1) {
+						set(PROFILE_SESSION, new GUIPartsSwitcher(5, 0) {
 							{
 								setName("PROFILE_SESSION");
-								this.appendLast(new GUIParts() {
-									final GUIParts NPC_1ScrBtn = getSwitcherButton(NPC_1).setBGPaint(buttonPaint)
-											.setName("NPC_1ScrBtn").setBounds(75, 20, 60, 60),
-											NPC_2ScrBtn = getSwitcherButton(NPC_2).setBGPaint(buttonPaint)
-													.setName("NPC_2ScrBtn").setBounds(75, 90, 60, 60),
-											NPC_3ScrBtn = getSwitcherButton(NPC_3).setBGPaint(buttonPaint)
-													.setName("NPC_3ScrBtn").setBounds(75, 160, 60, 60),
-											NPC_4ScrBtn = getSwitcherButton(NPC_4).setBGPaint(buttonPaint)
-													.setName("NPC_4ScrBtn").setBounds(75, 230, 60, 60);
-									// setBGColor(Color.red);
-									// setBounds(70, 0, 360, 768);
-									{
-										// this.addNewLine(gameScrBtn, settingsScrBtn);
-										this.setName("PROFILE_MENU_TOP_TAB");
-										this.appendLast(NPC_1ScrBtn);
-										this.appendLast(NPC_2ScrBtn);
-										this.appendLast(NPC_3ScrBtn);
-										this.appendLast(NPC_4ScrBtn);
-										this.setBounds(70, 0, 70, 768);
-									}
-								});
-								set(NPC_1, new GUIParts() {
-									{
-										setName("NPC_1_PROFILE");
-										setBGColor(Color.lightGray);
-										setBounds(140, 0, 290, 768);
-									}
-								});
-								set(NPC_2, new GUIParts() {
-									{
-										setName("NPC_2_PROFILE");
-										setBGColor(Color.orange);
-										setBounds(140, 0, 290, 768);
-									}
-								});
-								set(NPC_3, new GUIParts() {
-									{
-										setName("NPC_3_PROFILE");
-										setBGColor(Color.pink);
-										setBounds(140, 0, 290, 768);
-									}
-								});
-								set(NPC_4, new GUIParts() {
-									{
-										setName("NPC_4_PROFILE");
-										setBGColor(Color.green);
-										setBounds(140, 0, 290, 768);
-									}
-								});
+								this.setBounds(70, 0, 70, 768);
+								// 一个人物一个框
+								for (int i = 0; i < PersonEnum.values().length; i++) {
+									final GUIParts NPC_Button = getSwitcherButton(i)
+											.setBGImage("thhimage/veg_leaf2.png").setName("NPC_1ScrBtn")
+											.setBounds(75, 20 + i * 70, 60, 60);
+									this.appendLast(NPC_Button);
+									final GUIParts NPC_Profile = new GUIParts().setName("NPC_PROFILE")
+											.setBGImage(PersonEnum.values()[i].personIcon).setBounds(140, 0, 290, 768);
+									set(i, NPC_Profile);
+									// set(i, new GUIParts() {
+									// {
+									// setName("NPC_PROFILE");
+									// setBGImage(PersonEnum.values()[i].personIcon);
+									// setBounds(140, 0, 290, 768);
+									// }
+									// });
+								}
+								// this.appendLast(new GUIParts() {
+								// final GUIParts NPC_1ScrBtn =
+								// getSwitcherButton(NPC_1).setBGImage("thhimage/veg_leaf2.png")
+								// .setName("NPC_1ScrBtn").setBounds(75, 20, 60, 60),
+								// NPC_2ScrBtn =
+								// getSwitcherButton(NPC_2).setBGImage("thhimage/veg_leaf2.png")
+								// .setName("NPC_2ScrBtn").setBounds(75, 90, 60, 60),
+								// NPC_3ScrBtn =
+								// getSwitcherButton(NPC_3).setBGImage("thhimage/veg_leaf2.png")
+								// .setName("NPC_3ScrBtn").setBounds(75, 160, 60, 60),
+								// NPC_4ScrBtn =
+								// getSwitcherButton(NPC_4).setBGImage("thhimage/veg_leaf2.png")
+								// .setName("NPC_4ScrBtn").setBounds(75, 230, 60, 60);
+								// // setBGColor(Color.red);
+								// // setBounds(70, 0, 360, 768);
+								// {
+								// // this.addNewLine(gameScrBtn, settingsScrBtn);
+								// this.setName("PROFILE_MENU_TOP_TAB");
+								// this.appendLast(NPC_1ScrBtn);
+								// this.appendLast(NPC_2ScrBtn);
+								// this.appendLast(NPC_3ScrBtn);
+								// this.appendLast(NPC_4ScrBtn);
+								// this.setBounds(70, 0, 70, 768);
+								// }
+								// });
+								// set(NPC_1, new GUIParts() {
+								// {
+								// setName("NPC_1_PROFILE");
+								// setBGColor(Color.lightGray);
+								// setBounds(140, 0, 290, 768);
+								// }
+								// });
+								// set(NPC_2, new GUIParts() {
+								// {
+								// setName("NPC_2_PROFILE");
+								// setBGColor(Color.orange);
+								// setBounds(140, 0, 290, 768);
+								// }
+								// });
+								// set(NPC_3, new GUIParts() {
+								// {
+								// setName("NPC_3_PROFILE");
+								// setBGColor(Color.pink);
+								// setBounds(140, 0, 290, 768);
+								// }
+								// });
+								// set(NPC_4, new GUIParts() {
+								// {
+								// setName("NPC_4_PROFILE");
+								// setBGColor(Color.green);
+								// setBounds(140, 0, 290, 768);
+								// }
+								// });
 							}
 						});
 
@@ -198,7 +221,8 @@ public class GamePageSwitcher extends GUIPartsSwitcher {
 						set(SAVE_SESSION, new GUIParts() {
 							{
 								setName("SAVE_SESSION");
-								setBGColor(Color.pink);
+								// setBGColor(Color.pink);
+								setBGImage("thhimage/SaveBar.png");
 								setBounds(70, 0, 360, 768);
 							}
 						});
